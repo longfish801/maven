15957adfa38ce24b9b0d131ec27e32ac572444de
io.github.longfish801 <22591607+longfish801@users.noreply.github.com>
2022-02-21 19:28:10 +0900

    tpac 0.3.10 release

--- asset/script.js
+++ asset/script.js
@@ -1,5 +1,5 @@
 // bootstrapのJavaScriptファイルを読みこむ
-fetch('bootstrap-5.0.2/bootstrap.bundle.min.js')
+fetch('/maven/asset/bootstrap-5.0.2/bootstrap.bundle.min.js')
 	.then(response => response.text())
 	.then(text => eval(text))
 
@@ -8,5 +8,7 @@ fetch('bootstrap-5.0.2/bootstrap.bundle.min.js')
 // 現在のページが index.htmlなら上位のパスへ遷移する。
 function backPage(){
 	let url = window.location.href;
+	let anchorIdx = url.indexOf('#');
+	if (anchorIdx >= 0) url = url.substring(0, url.indexOf('#'));
 	window.location.href = (url.endsWith('/') || url.endsWith('index.html'))? '..' : 'index.html';
 }
--- src/markdown/index.md
+++ src/markdown/index.md
@@ -8,6 +8,10 @@
 
 ## ライブラリ
 
+[tpac](tpac/)
+: 複数行のテキストを含む文書を操作します。
+: いくつもの短い文章やスクリプトをひとつのファイルに整理したいときに便利です。
+
 [gonfig](gonfig/)
 : 設定を静的に保持します。
 : groovy.util.ConfigSlurperを java.util.ResourceBundleのように利用できます。
